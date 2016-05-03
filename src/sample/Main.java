a19979d3a570543e96a5f2f5875d22b9f2c8f1a4
mati199oo <mati199oo@gmail.com>
2016-05-03 16:50:43 +0200

    Finished javadoc and got rid off magic numbers.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -16,10 +16,12 @@ public class Main {
         frame.setDefaultCloseOperation(3);
         frame.setTitle("Thermometer");
 
-        final TermometerPanel panel = new TermometerPanel();
+        final TermometerPanel panel = new TermometerPanel("Temperature 1", 0.0, 200.0);
+        final TermometerPanel panel2 = new TermometerPanel("Temperature 2", 0.0, 100.0);
 
-        frame.getContentPane().add(panel, BorderLayout.CENTER);
-        frame.setSize(700, 490);
+        frame.getContentPane().add(panel, BorderLayout.WEST);
+        frame.getContentPane().add(panel2, BorderLayout.EAST);
+        frame.setSize(1400, 490);
         frame.setVisible(true);
         frame.setResizable(false);
 
@@ -29,11 +31,12 @@ public class Main {
                 Random random = new Random();
                 while(true){
                     try{
-                        Thread.sleep(1000);
+                        Thread.sleep(random.nextInt(5000));
                     } catch (InterruptedException e){
                         e.printStackTrace();
                     }
-                    panel.setTemperature(random.nextDouble() * 100.0);
+                    panel.setTemperature(random.nextDouble() * 200.0);
+                    panel2.setTemperature(random.nextDouble() * 200.0);
                 }
             }
         });
--- src/sample/TermometerPanel.java
+++ src/sample/TermometerPanel.java
@@ -12,23 +12,59 @@ import java.awt.*;
  */
 public class TermometerPanel extends JPanel implements Runnable {
 
-    private static final int THREAD_SLEEP = 1000;
-    private static final double MIN_TEMPERATURE = 0.0;
-    private static final double MAX_TEMPERATURE = 100.0;
+    private static final int THREAD_SLEEP = 100;
     private static final int W = 700;
     private static final int H = 450;
+    private static final double HALF_MEASURE = 0.5;
+    private static final double THREE_FOURTH_MEASURE = 0.75;
+    private static final int FIRST_SUBRANGE = 0;
+    private static final int SECOND_SUBRANGE = 1;
+    private static final int THIRD_SUBRANGE = 2;
+
+    private final double MIN_TEMPERATURE;
+    private final double MAX_TEMPERATURE;
 
     private ThermometerPlot thermometerPlot = new ThermometerPlot();
 
-    public TermometerPanel() {
+    /**
+     * Constructor for TermometerPanel, creates panel in which is instance of Termometer Chart to display temperature.
+     * It has method to set current value of temperature to display.
+     * @param title - is the title of chart to be displayed
+     * @param min - is minimum of temperature range
+     * @param max - is maximum of temperature range
+     */
+    public TermometerPanel(String title, double min, double max) {
+        MIN_TEMPERATURE = min;
+        MAX_TEMPERATURE = max;
         try {
-            thermometerInit();
+            thermometerInit(title);
         }
         catch (Exception ex) {
             ex.printStackTrace();
         }
     }
 
+    /**
+     * Private method for initializing thermometer parameters like title, range, subranges and colors of subranges.
+     * @param title - is the title of chart to be displayed
+     */
+    private void thermometerInit(String title){
+        thermometerPlot.setSubrange(FIRST_SUBRANGE, MIN_TEMPERATURE, HALF_MEASURE * MAX_TEMPERATURE);
+        thermometerPlot.setSubrange(SECOND_SUBRANGE, HALF_MEASURE * MAX_TEMPERATURE, THREE_FOURTH_MEASURE * MAX_TEMPERATURE);
+        thermometerPlot.setSubrange(THIRD_SUBRANGE, THREE_FOURTH_MEASURE * MAX_TEMPERATURE, MAX_TEMPERATURE);
+        thermometerPlot.setSubrangePaint(FIRST_SUBRANGE, Color.green.darker());
+        thermometerPlot.setSubrangePaint(SECOND_SUBRANGE, Color.orange);
+        thermometerPlot.setSubrangePaint(THIRD_SUBRANGE, Color.red.darker());
+        thermometerPlot.setLowerBound(MIN_TEMPERATURE);
+        thermometerPlot.setUpperBound(MAX_TEMPERATURE);
+        JFreeChart chart = new JFreeChart(title, JFreeChart.DEFAULT_TITLE_FONT, thermometerPlot, true);
+        this.add(new ChartPanel(chart, W, H, W, H, W, H, false, true, true, true, true, true));
+    }
+
+    /**
+     * Is public method that allows to set the temperature of thermometer. Takes double and casts it into int.
+     * @param temperature - is the temperature to be set.
+     */
     public void setTemperature(double temperature){
         if (temperature > MAX_TEMPERATURE){
             temperature = MAX_TEMPERATURE;
@@ -39,24 +75,10 @@ public class TermometerPanel extends JPanel implements Runnable {
         thermometerPlot.setDataset(defaultValueDataset);
     }
 
-    public void setTemperature(int temperature){
-        if (temperature > (int)MAX_TEMPERATURE){
-            temperature = (int)MAX_TEMPERATURE;
-        } else if (temperature < (int)MIN_TEMPERATURE){
-            temperature = (int)MIN_TEMPERATURE;
-        }
-        DefaultValueDataset defaultValueDataset = new DefaultValueDataset(temperature);
-        thermometerPlot.setDataset(defaultValueDataset);
-    }
-
-    private void thermometerInit(){
-        thermometerPlot.setSubrangePaint(0, Color.green.darker());
-        thermometerPlot.setSubrangePaint(1, Color.orange);
-        thermometerPlot.setSubrangePaint(2, Color.red.darker());
-        JFreeChart chart = new JFreeChart("Temperature", JFreeChart.DEFAULT_TITLE_FONT, thermometerPlot, true);
-        this.add(new ChartPanel(chart, W, H, W, H, W, H, false, true, true, true, true, true));
-    }
-
+    /**
+     * Inner method that runs after initializing class because it's swing component and is controlled by Swing Event Scheduler.
+     * It repaints panel with thermometer every 100 miliseconds.
+     */
     @Override
     public void run(){
         while(true){
